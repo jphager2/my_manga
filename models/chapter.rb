e01e6a89ab96522a03679de36812948df2fc7164
jphager2 <jphager2@gmail.com>
2017-09-10 01:31:52 +0200

    get chapter from md_hash

--- models/chapter.rb
+++ models/chapter.rb
@@ -4,12 +4,15 @@ class Chapter < ActiveRecord::Base
   scope :read, Proc.new { where(read: true) }
   scope :unread, Proc.new { where(read: false) }
 
-  def self.from_md(md_chapter)
-    uri = md_chapter.uri
-    name = md_chapter.name
-    chapter = md_chapter.chapter
+  def self.from_md(md_hash)
+    uri = md_hash.uri
+    name = md_hash.name
+    site = md_hash.site
 
-    find_or_create_by(name: name, uri: uri, number: chapter)
+    adapter = Mangdown.adapter!(uri, site, nil, name)
+    number = adapter.chapter[:chapter]
+
+    find_or_create_by(name: name, uri: uri, number: number)
   end
 
   def to_md
